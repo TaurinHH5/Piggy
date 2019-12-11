87726aae6004afb00746873f5d3448f6cd8fb0b2
Taurin Haddon-Harris <haddonharrist@gilmour.org>
2019-12-11 14:45:19 -0500

    fix postion

--- student.py
+++ student.py
@@ -34,7 +34,7 @@ class Piggy(PiggyParent):
         menu = {"n": ("Navigate", self.nav),
                 "d": ("Dance", self.dance),
                 "o": ("Obstacle count", self.obstacle_count),
-                "h": ("Hold Position", self.hold_position),
+                "h": ("Hold position", self.hold_position),
                 "c": ("Calibrate", self.calibrate),
                 "q": ("Quit", self.quit)
                 }
